a49639d0ecb16280cd6380c9002ecc3b9d996f87
Ayushmaan2001 <ayushmaanjangid2002@gmail.com>
2023-04-20 11:16:27 +0530

    changes-1

---
+++ src/App.tsx
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import DataStreamer, { ServerRespond } from './DataStreamer';
+import Graph from './Graph';
+import './App.css';
+
+/**
+ * State declaration for <App />
+ */
+//like datatypes
+interface IState {
+  data: ServerRespond[],
+  showGrap: boolean
+}
+
+/**
+ * The parent element of the react app.
+ * It renders title, button and Graph react element.
+ */
+class App extends Component<{}, IState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      data: [],
+      showGrap: false
+    };
+  }
+
+  /**
+   * Render Graph react component with state.data parse as property data
+   */
+  renderGraph() {
+    if(this.state.showGrap)
+      return (<Graph data={this.state.data}/>)
+  }
+
+  /**
+   * Get new data from server and update the state with the new data
+   */
+  getDataFromServer() {
+    let x = 0;
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        this.setState({ 
+          data:serverResponds,
+          showGrap:true
+        });
+      });
+      x++;
+      if(x > 1000){
+        clearInterval(interval);
+      }
+    },100)
+    
+  }
+
+  /**
+   * Render the App react component
+   */
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Bank & Merge Co Task 2
+        </header>
+        <div className="App-content">
+          <button className="btn btn-primary Stream-button"
+            onClick={() => {this.getDataFromServer()}}>
+            Start Streaming Data
+          </button>
+          <div className="Graph">
+            {this.renderGraph()}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
